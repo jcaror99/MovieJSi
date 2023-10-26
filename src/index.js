fbcdd65814d0cb01f259e85329fbb298da45fa26
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2023-10-26 00:05:03 -0500

    pageColorBar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import { fetch } from './settings/settings.js';
 
-window.addEventListener('load', () => {
-  fetch(1)
-    .then(ok => console.log(ok))
-    .catch(error => console.log(error));
-});
+const pagesBar = document.querySelector('.pages');
+
+function pagesHtmlBar(startValue = 5) {
+  const initialRangeArray = [];
+  for (let i = 0; i < startValue; i += 1) {
+    initialRangeArray.push(`<li class="pages_item">${i + 1}</li>`);
+  }
+  const renderBar = initialRangeArray.join('');
+  return (pagesBar.innerHTML = renderBar);
+}
+
+pagesHtmlBar();
+
+// window.addEventListener('load', () => {
+//   fetch(1)
+//     .then(ok => {
+
+//       console.log(ok);
+//     })
+//     .catch(error => console.log(error));
+// });

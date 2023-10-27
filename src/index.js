2f7532439faae150e667a7ca3c446e234c7b1bf7
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2023-10-26 19:53:48 -0500

    requestPage

--- src/index.js
+++ src/index.js
@@ -2,22 +2,36 @@ import { fetch } from './settings/settings.js';
 
 const pagesBar = document.querySelector('.pages');
 
+const requestFetch = page => {
+  fetch(page)
+    .then(ok => {
+      console.log(ok);
+    })
+    .catch(error => console.log(error));
+};
+
 function pagesHtmlBar(startValue = 5) {
   const initialRangeArray = [];
   for (let i = 0; i < startValue; i += 1) {
-    initialRangeArray.push(`<li class="pages_item">${i + 1}</li>`);
+    initialRangeArray.push(
+      `<li data-id="${i + 1}" class="pages_item"><a href="javascript:void(0)">${
+        i + 1
+      }</a></li>`
+    );
   }
   const renderBar = initialRangeArray.join('');
   return (pagesBar.innerHTML = renderBar);
 }
 
-pagesHtmlBar();
+pagesBar.addEventListener('click', e => {
+  if (e.target.tagName === 'A') {
+    const pageId = e.target.parentElement.getAttribute('data-id');
+    requestFetch(pageId);
+  }
+});
 
-// window.addEventListener('load', () => {
-//   fetch(1)
-//     .then(ok => {
-
-//       console.log(ok);
-//     })
-//     .catch(error => console.log(error));
+// window.addEventListener('load', e => {
+//   console.log(e);
 // });
+
+pagesHtmlBar();
